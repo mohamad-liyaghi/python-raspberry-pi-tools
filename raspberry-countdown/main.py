cc7c7bcc352c68462456960ee44004d44bb922b8
Ml06py <liaghimohamad69@gmail.com>
2022-05-05 17:31:53 +0430

    Count down added

--- raspberry-countdown/main.py
+++ raspberry-countdown/main.py
@@ -14,26 +14,50 @@ start_button = Pin(12, Pin.IN, Pin.PULL_DOWN)
 stop_button = Pin(13, Pin.IN, Pin.PULL_DOWN)
 add_button = Pin(14, Pin.IN, Pin.PULL_DOWN)
 reset_button = Pin(15, Pin.IN, Pin.PULL_DOWN)
+ 
+# time
+minuet = 0
+second = 0
+status = False
 
 while True:
-    status_led.value(1)
-    one_forth.value(1)
-    two_forth.value(1)
-    three_forth.value(1)
-    four_forth.value(1)
+    
     if reset_button.value():
         time.sleep(0.20)
-        print("1")
+        minuet = 0
+        print(minuet)
+        
     elif add_button.value():
         time.sleep(0.20)
-        print("2")
-    elif stop_button.value():
-        time.sleep(0.20)
-        print("3")
+        minuet += 1
+        print(minuet)
+        
     elif start_button.value():
         time.sleep(0.20)
-        print("4")
-    else:
-        pass
+        status = True
+        print("Started")
+    
+    while status:
+        status_led.value(1)
+        if minuet == 0 and second == 0:
+            status = False
+            status_led.value(0)
+            print("Done")
+            break
         
+        if minuet != 0 and second == 0:
+            second = 60
+            minuet -= 1
+            
+        if stop_button.value():
+            time.sleep(0.20)
+            status = False
+            status_led.value(0)
+            print("Stoped")
+            break
+        
+        second -= 1
+        time.sleep(1)
+        print(f"{minuet}:{second}")
+
 
